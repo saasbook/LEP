b313b1fb4009b42ef29bb737067876417dcd84ae
saasbook <saasbook@saasbook.(none)>
2015-03-03 07:47:23 -0400

    set up skeleton for step definitions, need to resolve ambiguities :(

---
+++ features/step_definitions/user_steps.rb
@@ -0,0 +1,17 @@
+# given step definitions
+Given /^I am (not )?authenticated$/ do |unauthenticated|
+end
+
+Given /^I am (not )?an admin$/ do |not_admin|
+end
+
+Given /^I am on the LEP homepage$/ do
+end
+
+# When step definitions
+When /^I go to the LEP page$/ do
+end
+
+# Then step definitions
+Then /^I should be at the LEP (.*) home page$/ do |user_type|
+end
