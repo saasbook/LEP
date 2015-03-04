da63d84e4589d210db74b6eec8ccef1896e08aaa
tbehailu <tsionbehailu@gmail.com>
2015-03-03 19:17:16 -0800

    added example entry into users db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+@user = User.create({first_name: 'Jane', 
+						last_name: 'Doe', 
+						sid: '12345678', 
+						email: 'example@gmail.com',
+						academic_title: 'Undergraduate',
+						major: 'Computer Science', 
+						residency: 'Domestic', 
+						gender: 'Female', 
+						gender_preference: 'Female',
+						fluent_languages: 'English', 
+						lang_additional_info: '',
+						first_lang_preference: 'French', 
+						first_lang_proficiency: 'elementary', 
+						second_lang_preference: 'Arabic', 
+						second_lang_proficiency: 'elementary',
+						time_preference: 'Monday', 
+						time_additional_info: '',
+						user_motivation: 'I will be studying abroad.', 
+						user_plan: 'I will set a regular meeting time with them.'})
