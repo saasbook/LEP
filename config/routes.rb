81fe8b323beb9d0263977a71db46e1147de4c82e
tbehailu <tsionbehailu@gmail.com>
2015-03-03 19:19:08 -0800

    moved resources lines to the top of file

--- config/routes.rb
+++ config/routes.rb
@@ -62,7 +62,4 @@ LEP::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
 
-  resources :users
-  resources :groups
-
 end
