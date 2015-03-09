b257a11be43d3df23768995ee971e6c5da0f2bb2
robynzhang <robynz@berkeley.edu>
2015-03-09 18:59:13 -0300

    fix bugs in user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,18 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
-    @auth = request.env['omniauth.auth']['credentials']
-    @email = request.env['omniauth.auth']['info']['email']
-    #@user = User.create(params[:user])
-    #redirect_to users_path #redirect to show action
+    #@auth = request.env['omniauth.auth']['credentials']
+    #@email = request.env['omniauth.auth']['info']['email']
+    @user = User.new(user_params)
+    #@user = User.create!(params[:user])
+    @user.application = true
+    @user.is_admin = false
+    #redirect_to user_path(params[:id]) #redirect to show action
+    redirect_to user_path(@user)
   end
 
   def index
@@ -27,14 +32,28 @@ class UsersController < ApplicationController
 
   def home
     email = request.env['omniauth.auth']['info']['email']
-    @user = User.where(email: email)
+    print "*****************EMAIL******************", email
+    @user = User.where(:email => email)
+    print "*****************FOUND EMAIL******************", @user.pluck(:email)
+    print "***********************************"
     #no application yet
     if @user.blank?
       redirect_to new_user_path
     else
       is_admin = @user.pluck(:is_admin)
-      redirect_to user_path(id: @user.id, admin: is_admin)
+      redirect_to user_path(params[:id]) if not is_admin
+      #redirect_to admin_path if is_admin 
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :sid, :email, :academic_title, :major, :residency,
+                                :gender, :gender_preference, :fluent_languages, :lang_additional_info,
+                                :first_lang_preference, :first_lang_proficiency, :second_lang_preference,
+                                :second_lang_proficiency, :time_preference, :time_additional_info, 
+                                :user_motivation, :user_plan, :is_admin, :application)
   end
 
 end
