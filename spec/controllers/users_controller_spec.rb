27d16ca8adaddb1a45e1133d2e25ea5c7d4e1757
Shana Hu <shanawho@gmail.com>
2015-03-09 00:06:12 -0300

    admin controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
 
 describe UsersController do
+  describe '#index' do
+    it 'renders index template' do
+      get :index
+      expect(response).to render_template('index')
+    end
+  end
+
 end
