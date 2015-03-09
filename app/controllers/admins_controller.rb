a5c3837d3cf165aa4df9f4f0f5783ceb11faa432
Shana Hu <shanawho@gmail.com>
2015-03-09 01:32:43 -0300

    admin files and view

---
+++ app/controllers/admins_controller.rb
@@ -0,0 +1,33 @@
+class AdminsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @auth = request.env['omniauth.auth']['credentials']
+    @email = request.env['omniauth.auth']['info']['email']
+    #@user = User.create(params[:user])
+    #redirect_to users_path #redirect to show action
+  end
+
+  def index
+  	@users = User.all
+
+  end
+
+  def show
+  	@user = User.find(params[:id])
+  end
+
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admins_path
+  end
+
+
+end
