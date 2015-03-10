2d4a5053664f414499313b86d1b92923a876cf92
Shana Hu <shanawho@gmail.com>
2015-03-09 21:23:30 -0300

    blue h1

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150309053124) do
+ActiveRecord::Schema.define(version: 20150304021522) do
 
   create_table "users", force: true do |t|
     t.string   "first_name"
@@ -35,8 +35,6 @@ ActiveRecord::Schema.define(version: 20150309053124) do
     t.string   "user_plan"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "is_admin"
-    t.boolean  "application"
   end
 
 end
