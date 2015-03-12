34e3d5d1c8e5d2d3390bb61145655f76e494a1ce
swoo <swchoi727@gmail.com>
2015-03-12 16:17:46 -0700

    removed application

--- db/schema.rb
+++ db/schema.rb
@@ -33,7 +33,6 @@ ActiveRecord::Schema.define(version: 20150311225433) do
     t.string   "time_additional_info"
     t.string   "user_motivation"
     t.string   "user_plan"
-    t.boolean  "application"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "admin",                   default: false
