ef89f96af3e9ae464aa98075bb94cfc55d5e423d
tbehailu <tsionbehailu@gmail.com>
2015-03-12 20:00:10 -0700

    fill in controller methods

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,18 +4,30 @@ class GroupsController < ApplicationController
   end
 
   def create
-    
+    @group = Group.new(group_params)
+    redirect_to group_path(@group)
   end
 
   def index
-    @user = User.find(session[:id])
     @groups = Group.all
   end
 
+  def edit
+    @group = Group.find(params[:id])
+  end
+
   def show
+    @group = Group.find(params[:id])
   end
 
   def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    redirect_to group_path
+  end
+
+  def group_params
+    params.require(:group).permit(:id, :languages)
   end
 
 end
