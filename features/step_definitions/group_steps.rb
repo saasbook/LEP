1a5f6ecef8fe10a752d110a45b37ab011574d6e2
swoo <swchoi727@gmail.com>
2015-03-12 22:06:02 -0700

    adding group steps

---
+++ features/step_definitions/group_steps.rb
@@ -0,0 +1,26 @@
+Given /^the following groups exist$/ do |groups_hash|
+  groups_hash.hashes.each do |group|
+    Group.create({members: group['members'], languages: group['languages']})
+  end
+end
+
+Given /^the following users exist$/ do |users_hash|
+  users_hash.hashes.each do |user|
+    User.create({first_name: user['first_name'], admin: user['admin']})
+  end
+end
+
+
+Then /^I should see all users$/ do
+  User.all.each do |user|
+    puts user['first_name']
+    assert page.has_content?(user['first_name'])
+  end
+end
+
+Then /^I should see all groups$/ do
+  Group.all.each do |group|
+    puts movie['title']
+    assert page.has_content?(group['title'])
+  end
+end
