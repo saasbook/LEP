3405e21dea6728daa548c06acc2f46521d29c614
Jason Jia <jason.jia1993@gmail.com>
2015-04-07 20:04:34 -0300

    Wrote rspec tests for student join/leave groups and increased coverage in other random tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   def check_email
     # Check that the user is properly logged in
     if session[:invalid_email]
-      flash[:warning] = "#{session[:invalid_email]} is not a valid email. \n Please logout and reauthenticate with a Berkeley email address."
+      flash[:warning] = "#{session[:invalid_email]} is not a valid email. \n Please Logout and reauthenticate with a Berkeley email address."
       redirect_to users_invalid_path
     end
   end
--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -1,11 +1,22 @@
 require 'spec_helper'
 
 describe GroupsController do
+  before :each do
+    @group = Group.create!(language: "German", day: "Tuesday", time: "2-3PM", location: "1 Evans")
+    @user = User.create!(first_name: 'Blah', admin: false)
+  end
+
   describe '#index' do
     it 'should render the #index template' do
-      @user = User.create(first_name: 'Blah', admin: false)
       get(:index, nil, {id: @user.id})
       expect(response).to render_template('index')
+    end
+  end
+
+  describe '#new' do
+    it "should assign @user" do
+      get(:new, {}, {id: @user.id})
+      assigns(:user).should eq(@user)
     end
   end
 
@@ -19,8 +30,29 @@ describe GroupsController do
 
   describe '#delete' do
     it 'should delete a group' do
-      @group = Group.create(language: "German", day: "Tuesday", time: "2-3PM", location: "1 Evans")
       get(:destroy, {id: @group.id})
+    end
+  end
+
+  describe '#join' do
+    it "should allow a student to join a group" do
+      get(:join, {:id => @group.id}, {:id => @user.id})
+      assigns(:group).should eq(@group)
+    end
+    it "should redirect to the groups path" do
+      get(:join, {:id => @group.id}, {:id => @user.id})
+      response.should redirect_to(groups_path)
+    end
+  end
+
+  describe '#leave' do
+    it "should allow a student to leave a group" do
+      get(:leave, {:id => @group.id}, {:id => @user.id})
+      assigns(:group).should eq(@group)
+    end
+    it "should redirect to the groups path" do
+      get(:join, {:id => @group.id}, {:id => @user.id})
+      response.should redirect_to(groups_path)
     end
   end
 
