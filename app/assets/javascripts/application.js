ee2451ab4d6beca0321bd73f5a963069ad2ba0cd
tbehailu <tsionbehailu@gmail.com>
2015-04-14 17:49:28 -0700

    others field for first_lang_preference, second_lang_preference, and group_language seem to be rendering on correctly

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,42 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+
+$(function(){
+    // $('select[name="group_language"]').change(function() {
+      // $('input[name="group_language_other"]').toggle(  $(this).val()=="Other" );
+      // if ($("#group_language").val()=="Other")
+
+    //Hide div w/id extra
+		$("#group_language_other").hide();
+		$("#first_lang_preference_other").hide();
+		$("#second_lang_preference_other").hide();
+
+    $('#group_language').click(function(){
+      console.log($(this).val());
+      if ($(this).val()=="other") {
+      	$("#group_language_other").show("fast");
+      } else {
+        $("#group_language_other").hide("fast");
+      }
+    });
+
+    $('#first_lang_preference').click(function(){
+      console.log($(this).val());
+      if ($(this).val()=="other") {
+      	$("#first_lang_preference_other").show("fast");
+      } else {
+        $("#first_lang_preference_other").hide("fast");
+      }
+    });
+
+    $('#second_lang_preference').click(function(){
+      console.log($(this).val());
+      if ($(this).val()=="other") {
+      	$("#second_lang_preference_other").show("fast");
+      } else {
+        $("#second_lang_preference_other").hide("fast");
+      }
+    });
+});
