2e2d5d6849ca4b7738b90778db4df19343333453
Iris Wang <iriswang@berkeley.edu>
2015-04-29 07:06:28 -0300

    fixed timesheets code to make it cleaner

--- app/controllers/timesheets_controller.rb
+++ app/controllers/timesheets_controller.rb
@@ -8,15 +8,27 @@ class TimesheetsController < ApplicationController
       @user = User.where(:id => session[:id])
       @admin = @user.pluck(:admin)[0]
       @timesheet = Timesheet.where(:id => params[:id])[0]
-      if !@admin 
-        if (params[:user_id].to_s != session[:id].to_s)
-          redirect_to user_timesheets_path(session[:id])
-        end
-        if (not @timesheet.nil?) && (params[:user_id] != @timesheet.user_id.to_s)
-          redirect_to user_timesheets_path(session[:id])
-        end
-      end
     end
+    if !@admin && (not (user_permitted && timer_info_permitted))
+      redirect_to user_timesheets_path(session[:id])
+    end
+  end
+  
+  def user_permitted
+    if (params[:user_id].to_s != session[:id].to_s)
+      return false
+    end
+    return true
+  end
+
+  def timer_info_permitted
+    if (@timesheet.nil?) && (not params[:id].nil?)
+      return false
+    end
+    if (not @timesheet.nil?) && (params[:user_id] != @timesheet.user_id.to_s)
+      return false
+    end
+    return true
   end
 
   def new
@@ -26,18 +38,26 @@ class TimesheetsController < ApplicationController
     @method = 'post'
   end
 
+  def update_database(flash_msg)
+    begin
+      @timesheet.save!
+      flash[:notice] = flash_msg
+      redirect_to user_timesheets_path(@user)
+      return true
+    rescue ActiveRecord::RecordInvalid => e
+      flash[:warning] = e.record.errors.to_a[0]
+      return false
+    end
+  end
+
   def create
     @user = User.find(session[:id])
     if check_params
       @timesheet = Timesheet.new(timesheet_params)
       @timesheet.user_id = @user.id
-      begin
-        @timesheet.save!
-        flash[:notice] = "You have successfully added a timesheet."
-        redirect_to user_timesheets_path(@user)
+      flash_msg = "you have successfully added a timesheet."
+      if update_database(flash_msg)
         return
-      rescue ActiveRecord::RecordInvalid => e
-        flash[:warning] = e.record.errors.to_a 
       end
     end
     redirect_to new_user_timesheet_path(@user)
@@ -61,20 +81,16 @@ class TimesheetsController < ApplicationController
 
   def update
     @timesheet = Timesheet.find(params[:id]) 
-    @timesheet.update(timesheet_params)
     if check_params
-      begin
-        @timesheet.save!
-        flash[:notice] = "You have successfully edited a timesheet."
-        redirect_to user_timesheets_path(@user)
+      @timesheet.update(timesheet_params)
+      flash_msg = "You have successfully edited a timesheet."
+      if update_database(flash_msg)
         return
-      rescue ActiveRecord::RecordInvalid => e
-        flash[:warning] = e.record.errors.to_a 
       end
-    else
-      redirect_to edit_user_timesheet_path(@user, @timesheet)
     end
+    redirect_to edit_user_timesheet_path(@user, @timesheet)
   end
+
 
   def destroy
     @timesheet = Timesheet.find(params[:id])
