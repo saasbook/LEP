51f4cad62b500ddba2a9a3ef9ce9fef5b0b30fe5
swoo <swchoi727@gmail.com>
2015-04-30 13:46:40 -0700

    changed pair spec

--- spec/models/pair_spec.rb
+++ spec/models/pair_spec.rb
@@ -1,12 +1,98 @@
 require 'spec_helper'
 
 describe Pair do
-  
+ 
   before :each do
-    @user1 = User.create(id: 2)
-    @user2 = User.create(id: 3)
-    @user3 = User.create(id: 4)
-    @pair1 = Pair.create(id: 1, member1: @user1.id.to_s, member2: @user2.id.to_s, member3: '')
+
+    @user1 = User.create(
+      id: 1,
+      first_name: "1", 
+      last_name: "LASTNAME", 
+      sid: "#{Random.new.rand(00000000..99999999)}", 
+      email: "example@gmail.com", 
+      academic_title: "DEFAULT", 
+      major: "MAJOR", 
+      residency: "DEFAULT",
+      gender: "GENDER", 
+      gender_preference: "DEFAULT", 
+      fluent_languages: ["english"], 
+      fluent_languages_other: "DEFAULT", 
+      lang_additional_info: "DEFAULT",
+      first_lang_preference: "DEFAULT",
+      first_lang_proficiency: "DEFAULT", 
+      second_lang_preference: "DEFAULT",
+      second_lang_proficiency: "DEFAULT",
+      group_leader: 'No',
+      time_preference: ["monday"],
+      hours_per_week: "0",
+      user_motivation: "DEFAULT",
+      user_plan: "DEFAULT",
+      admin: false,
+      active: true,
+      facilitator: false,
+      group_language: "DEFAULT"
+    )
+    
+    @user2 = User.create(
+      id: 2,
+      first_name: "2", 
+      last_name: "LASTNAME", 
+      sid: "#{Random.new.rand(00000000..99999999)}", 
+      email: "example@gmail.com", 
+      academic_title: "DEFAULT", 
+      major: "MAJOR", 
+      residency: "DEFAULT",
+      gender: "GENDER", 
+      gender_preference: "DEFAULT", 
+      fluent_languages: ["english"], 
+      fluent_languages_other: "DEFAULT", 
+      lang_additional_info: "DEFAULT",
+      first_lang_preference: "DEFAULT",
+      first_lang_proficiency: "DEFAULT", 
+      second_lang_preference: "DEFAULT",
+      second_lang_proficiency: "DEFAULT",
+      group_leader: 'No',
+      time_preference: ["monday"],
+      hours_per_week: "0",
+      user_motivation: "DEFAULT",
+      user_plan: "DEFAULT",
+      admin: false,
+      active: true,
+      facilitator: false,
+      group_language: "DEFAULT"
+    )
+
+    @user3 = User.create(
+      id: 3,
+      first_name: "3", 
+      last_name: "LASTNAME", 
+      sid: "#{Random.new.rand(00000000..99999999)}", 
+      email: "example@gmail.com", 
+      academic_title: "DEFAULT", 
+      major: "MAJOR", 
+      residency: "DEFAULT",
+      gender: "GENDER", 
+      gender_preference: "DEFAULT", 
+      fluent_languages: ["english"], 
+      fluent_languages_other: "DEFAULT", 
+      lang_additional_info: "DEFAULT",
+      first_lang_preference: "DEFAULT",
+      first_lang_proficiency: "DEFAULT", 
+      second_lang_preference: "DEFAULT",
+      second_lang_proficiency: "DEFAULT",
+      group_leader: 'No',
+      time_preference: ["monday"],
+      hours_per_week: "0",
+      user_motivation: "DEFAULT",
+      user_plan: "DEFAULT",
+      admin: false,
+      active: true,
+      facilitator: false,
+      group_language: "DEFAULT"
+    )
+
+    @pair1 = Pair.create!(member1: @user1.id.to_s, member2: @user2.id.to_s, member3: '')
+
   end
 
   describe 'check if a user is in a pair' do
@@ -31,24 +117,23 @@ describe Pair do
 
   describe 'remove_user_from_pair' do
     it 'should remove a specified user from a pair' do
-      expect(@pair1.member1).to eq('2')
-      Pair.remove_user_from_pair(1, @user1.id)
-      expect(Pair.find(1).member1).eql?('')
-      Pair.add_user_to_pair(1, @user1.id)
-      expect(Pair.find(1).member1).eql?('1')
-      Pair.remove_user_from_pair(1, @user2.id)
-      expect(Pair.find(1).member2).eql?('')
-      Pair.add_user_to_pair(1, @user2.id)
-      Pair.add_user_to_pair(1, @user3.id)
-      Pair.remove_user_from_pair(1, @user3.id)
-      expect(Pair.find(1).member3).eql?('')
+      Pair.remove_user_from_pair(@pair.id, @user1.id)
+      expect(Pair.find(@pair.id).member1).eql?('')
+      Pair.add_user_to_pair(@pair.id, @user1.id)
+      expect(Pair.find(@pair.id).member1).eql?(@user1.id)
+      Pair.remove_user_from_pair(@pair.id, @user2.id)
+      expect(Pair.find(@pair.id).member2).eql?('')
+      Pair.add_user_to_pair(@pair.id, @user2.id)
+      Pair.add_user_to_pair(@pair.id, @user3.id)
+      Pair.remove_user_from_pair(@pair.id, @user3.id)
+      expect(Pair.find(@pair.id).member3).eql?('')
     end
   end
 
   describe 'add_user_to_pair' do
     it 'should add a specified user to a pair' do
       Pair.add_user_to_pair(@pair1.id, @user3.id)
-      expect(Pair.find(@pair1.id).member3).to eq(@user3.id.to_s)
+      expect(Pair.find(@pair.id).member3).to eq(@user3.id.to_s)
     end
   end
 
